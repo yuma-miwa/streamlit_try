6e1375540f066cb3718c178300d8baaf2cdbf36a
Yuma Miwa <y.miwa0408@gmail.com>
2025-03-02 22:58:05 +0900

    URL追加

--- get_weather_forecast_by_steamlit.py
+++ get_weather_forecast_by_steamlit.py
@@ -5,36 +5,13 @@ import json
 
 
 # フロントエンドを表示
-streamlit.title("どこの天気が知りたい")
-question = streamlit.text_input("質問を入力")
-button = streamlit.button("質問する")
+streamlit.title("どこの天気が知りたい？")
+question = streamlit.text_input("地域コードを入力")
+button = streamlit.button("教えてもらう")
+areaCodeURL = streamlit.page_link("https://weather.tsukumijima.net/primary_area.xml", "地域コード")
+apiURL = streamlit.page_link("https://weather.tsukumijima.net/", "API仕様")
 
-# ボタンが押されたらナレッジベースを呼び出し
-# if button:
-#   response = bedrock.retrieve_and_generate(
-#     input={'text': question},
-#     retrieveAndGenerateConfiguration={
-#       'type': 'KNOWLEDGE_BASE',
-#       'knowledgeBaseConfiguration': {
-#         'knowledgeBaseId': 'XXXXXXXXXX', # ナレッジベースID
-#         'modelArn': 'anthropic.claude-3-5-sonnet-20240620-v1:0'}})
-
-if button:
-    # # 気象庁データの取得
-    # jma_url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
-    # jma_json = requests.get(jma_url).json()
-
-    # # 取得したいデータを選ぶ
-    # jma_date = jma_json[0]["timeSeries"][0]["timeDefines"][0]
-    # jma_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][0]
-    # #jma_rainfall = jma_json["Feature"][0]["Property"]["WeatherList"]["Weather"][0]["Rainfall"]
-    # # 全角スペースの削除
-    # jma_weather = jma_weather.replace('　', '')
-
-    # # 回答を表示
-    # #   streamlit.write(response['output']['text'])
-    # streamlit.write(jma_weather)
-    
+if button:    
     API_URL = "https://weather.tsukumijima.net/api/forecast/city/{0}"
 
     req_url = API_URL.format(question)
